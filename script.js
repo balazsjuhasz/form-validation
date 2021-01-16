cbf37e89c4aa48a43c38a519c55c3fafc73c0918
Balazs Juhasz <juhaszbal@gmail.com>
2021-01-16 14:38:32 +0100

    finish form validation

--- script.js
+++ script.js
@@ -5,20 +5,61 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
+let passwordsMatch = false;
 
 const validateForm = () => {
   // using Contraint API
   isValid = form.checkValidity();
   // Style main message for an error
-  message.textContent = 'Please fill out all fields.';
-  message.style.color = 'red';
-  messageContainer.style.borderColor = 'red';
+  if (!isValid) {
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    return;
+  }
+
+  // Check to see if passwords match
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = 'green';
+    password2El.style.borderColor = 'green';
+  } else {
+    passwordsMatch = false;
+    message.textContent = 'Make sure passwords match.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    password1El.style.borderColor = 'green';
+    password2El.style.borderColor = 'green';
+    return;
+  }
+  // If form is valid and passwords match
+  if (isValid && passwordsMatch) {
+    message.textContent = 'Successfully Registered!';
+    message.style.color = 'green';
+    messageContainer.style.borderColor = 'green';
+  }
+};
+
+const storeFormData = () => {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
+  // Do something with user data
+  console.log(user);
 };
 
 const processFormData = (e) => {
   e.preventDefault();
   // Validate Form
   validateForm();
+  // Submit Data if Valid
+  if (isValid && passwordsMatch) {
+    storeFormData();
+  }
 };
 
 // Event Listener
